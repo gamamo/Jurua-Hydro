7f3d6b27685d7e06ceb507fdc52232f74a5170f3
Gabriel Moulatlet <gamamo@utu.fi>
2016-11-30 14:26:48 +0200

    Importing these script from previous R project

---
+++ script_preparacao_sp_analises.R
@@ -0,0 +1,214 @@
+# Preparing data for analysis and creating moisture indexes #####
+# owner Gabriel Massaine Moulatlet
+# created in November of 2016
+# this script is to prepara the data to analyse in the Hydro paper of Jurua                                             
+# it is divided by groups: gingers, ferns and palms  because original tables
+# were too different. Each dataset required a special treatment.
+# as outputs, it creates a wide-format table for community analysis, and
+# tables with a moisture index
+
+
+#load the packages
+
+library(reshape2)
+library(vegan)
+
+################# zingibs #########################
+# organizing the zingib data
+# importing the data and set the working directory
+
+
+setwd("C:/workspace_Gabriel_Moulatlet/Hidrologia do jurua/analyses v1/dados floristicos originais")
+zingdata <- read.csv("Zing_jurua_database.csv",stringsAsFactors=FALSE)
+    str(zingdata)
+    zingdata$quant <- as.numeric(zingdata$quant)   # transform the species abundances in numeric
+    zingdata$id_1  <- tolower(zingdata$id_1)       # transform species id to lowercase to avoid spelling errors
+
+zingwide <- dcast(zingdata, transecto + sub_plot ~ id_1, value.var = "quant", fun.aggregate = sum)  #species table: long to wide
+  zingwide <- zingwide[,-which(colnames(zingwide)=="na")]         # delete subplots without species
+
+#change transects number to homogenized all datasets
+  transectsseq <- seq(738,777) 
+
+    #looping to change replace values  
+
+        for(i in unique(zingwide$transecto)){
+          nums <- strsplit(i, "[^[:digit:]]")   # split string non-digits
+          nums <- as.numeric(unlist(nums))      # convert string to numeric ("" become NA)
+          nums <- unique(nums[!is.na(nums)])    # remove NA and duplicates
+          zingwide[zingwide$transecto==i,"transecto"] <- transectsseq[nums]
+    }
+    
+
+  # write the table in .csv
+  
+write.csv(zingwide, "zingdata_wide_gmm_v1.csv")
+
+##########################  Moisture index for zing   #################################################
+
+# import species classification made by zing's specialist
+
+getwd()  # check the working directory
+
+zingclass <- read.csv("zing_jurua_baixio.csv", stringsAsFactors = F)
+  zingspnames <- colnames(zingwide[-c(1:2)])  # get the species names of the wide table
+  zingclass   <- cbind(zingspnames, zingclass[-1]) # and substitute the names that came in the imported file. Names order were previously checked
+      zingclass$MI <- as.numeric(zingclass$MI)     # transform MI values in numeric
+  
+# associate MI to the zingdata object
+      
+      zingdata$moistureindex <- NA # creat an empty colunm to the zingdata data.frame
+  
+# associate the index to the zingdata data.frame
+      
+  for(i in seq(nrow(zingclass))) {
+    zingdata[zingdata$id_1==zingclass$zingspnames[i],"moistureindex"] <- zingclass$MI[i]
+  }
+
+# cast the zingdata 
+      
+      zingwideMI <- dcast(zingdata, transecto + sub_plot ~ id_1, value.var = "moistureindex", fun.aggregate = sum)  #species table: long to wide
+      zingwideMI <- zingwideMI[,-which(colnames(zingwideMI)=="na")]                             # delete subplots without species
+      
+# to creat the moisture index, first species relative abundances will be calculated and 
+# then weighted by the moisture index. Finally, these values will be summed by subtransect
+# in order to have a index value for each subtransect
+      
+      zingwideMIsp <- zingwideMI[,-c(1:2)]
+      zingwideMIsp[zingwideMIsp>0] <- 2     # all the values higher than 2 will be turned to 2. This is because a species can have two occurrences
+                                        # in a sinlge subtransect, making the index 4.    
+      
+      
+      relabuzing <- decostand(zingwide[,-c(1:2)], method = "total",na.rm=T )
+      moistureindextablezing <- relabuzing*zingwideMIsp
+        moistureindextablezing <- cbind(zingwide[,c(1:2)],moistureindextablezing)
+      
+      write.csv(moistureindextablezing,"moistureindextablezing.csv")  # save a csv file
+      
+      MIzing <- cbind(moistureindextablezing[,c(1:2)],rowSums(moistureindextablezing[,-c(1:2)]))
+        colnames(MIzing)[3] <- "MI"   #change the name of the moisture index column
+      
+      write.csv(MIzing, "MIzing_persubunit.csv")  # save a csv file
+      
+      
+############################# ferns  ##########################
+# organizing ferns data
+# import files      
+# the first part of this code was created by H.T.     
+      
+#load the packages
+      
+library(reshape2)
+library(vegan)
+
+# The file with the old and new species codes should have columns named "Sp_old" and "Sp_new"
+# The file with transect data should have column with name "SpCode"
+
+setwd("C:/workspace_Gabriel_Moulatlet/Hidrologia do jurua/analyses v1/dados floristicos originais")
+dir()
+
+# change names as appropriate
+sp_data_name <- "Jurua data to analysis_ferns_from hanna.csv"  
+sp_lump_name <- "Jurua fern spp list 2013-10_03_v2.csv"
+
+# open the specified files
+sp_data      <- read.csv(sp_data_name,header = T, stringsAsFactors=FALSE)
+sp_lump_list <- read.csv(sp_lump_name,header = T, stringsAsFactors=FALSE)
+
+
+# replace old SpCodes with new ones
+for(i in seq(nrow(sp_lump_list))) {
+  sp_data[sp_data$SpCode==sp_lump_list$Sp_old[i],"SpCode"] <- sp_lump_list$Sp_new[i]
+}
+
+
+# Eliminate unidentified species observations
+sp_data <- sp_data[sp_data$SpCode!="",]
+
+# associate a moisture index and the epiphyte/non-epiphyte classification
+# creat two empty columns
+
+sp_data$moistureindex <- NA
+sp_data$habit         <- NA
+
+for(i in seq(nrow(sp_lump_list))) {
+  sp_data[sp_data$SpCode==sp_lump_list$Sp_old[i],"moistureindex"] <- sp_lump_list$index[i]
+}
+
+for(i in seq(nrow(sp_lump_list))) {
+  sp_data[sp_data$SpCode==sp_lump_list$Sp_old[i],"habit"] <- sp_lump_list$habit[i]
+}
+
+sp_data <- sp_data[sp_data$moistureindex!="",]
+
+write.csv(sp_data, "ferndataMI.csv")
+
+############################### creating a moisture index for ferns #############################
+
+widetable    <- dcast(sp_data, TrNumber + subunit ~ SpCode,value.var = "ind"           ,fun.aggregate = sum)
+widetableMI  <- dcast(sp_data, TrNumber + subunit ~ SpCode,value.var = "moistureindex" ,fun.aggregate = sum)
+
+
+widetableMIsp <- widetableMI[,-c(1:2)]
+widetableMIsp[widetableMIsp>0] <- 2
+widetableMI <- cbind(widetableMI[,c(1:2)], widetableMIsp)
+
+relabu <- decostand(widetable[,-c(1:2)], method = "total",na.rm=T )
+moistureindextable <- relabu*widetableMIsp
+moistureindextable <- cbind(widetableMI[,1:2],moistureindextable)
+MIferns <- cbind(widetableMI[,1:2], rowSums(moistureindextable[,-c(1:2)]))
+  colnames(MIferns)[3] <- "MI"  
+
+write.csv(moistureindextable,"moistureindextable.csv")
+write.csv(MIferns, "MIferns_persubunit.csv")
+
+
+########################### palms #############################
+# organizing the palm data
+# importing data
+
+getwd()
+
+palmdata  <- read.csv( "palms_jurua_subunit_gmm1.csv")
+palmclass <- read.csv( "Jurua_Palms_splist_habitatAff_gmm.csv")
+  palmclass$species <- as.character(palmclass$species)
+
+palmdatalong <- melt(palmdata, id.vars = c("TrNumber", "subunit"))
+
+# creat a empty column for the moiture value
+
+palmdatalong$moistureindex <- NA
+
+# associate the index to the palmdata data.frame
+
+for(i in seq(nrow(palmclass))) {
+  palmdatalong[palmdatalong$variable==palmclass$species[i],"moistureindex"] <- palmclass$moisture[i]
+}
+
+# cast the palmdata long
+
+palmdataMI <- dcast(palmdatalong, TrNumber + subunit ~ variable, value.var = "moistureindex", fun.aggregate = sum)  #species table: long to wide
+
+################### moisture index for palms ##########################################
+# to creat the moisture index, first species relative abundances will be calculated and 
+# then weighted by the moisture index. Finally, these values will be summed by subtransect
+# in order to have a index value for each subtransect
+
+palmdataMIsp <- palmdataMI[,-c(1:2)]
+palmdataMIsp[palmdataMIsp>0] <- 2     # all the values higher than 2 will be turned to 2. This is because a species can have two occurrences
+                                      # in a sinlge subtransect, making the index 4.    
+palmdataMIsp <- palmdataMIsp[,!is.na(colSums(palmdataMIsp))]
+
+# calculate de relative abundance and the index
+
+relabupalm <- decostand(palmdata [,-c(1:2)], method = "total",na.rm=TRUE )
+moistureindextablepalm <- relabupalm[,colnames(palmdataMIsp)]*palmdataMIsp   # exclude de NA columns of the relaabupalm
+moistureindextablepalm <- cbind(palmdata[,c(1:2)],moistureindextablepalm)
+
+write.csv(moistureindextablepalm,"moistureindextablepalm.csv")  # save a csv file
+
+MIpalm <- cbind(moistureindextablepalm[,c(1:2)],rowSums(moistureindextablepalm[,-c(1:2)]))
+colnames(MIpalm)[3] <- "MI"   #change the name of the moisture index column
+
+write.csv(MIpalm, "MIpalm_persubunit.csv")  # save a csv file
+
